9fd69ae2c94ee1fb883b41003dca674803a7127c
Jairo <jairoandre@gmail.com>
2022-01-07 20:33:41 -0300

    More study

--- particles/particles.go
+++ particles/particles.go
@@ -2,28 +2,50 @@ package particles
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"image/color"
 	"math/rand"
 )
 
 type Particle struct {
-	x float64
-	y float64
+	x   float64
+	y   float64
+	img *ebiten.Image
 }
 
 func (p *Particle) draw(screen *ebiten.Image) {
-	ebitenutil.DrawRect(screen, p.x, p.y, 1.0, 10.0, color.White)
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(p.x, p.y)
+	screen.DrawImage(p.img, op)
+}
+
+func (p *Particle) update() {
+	p.x += rand.Float64() - 0.5
+	p.y += rand.Float64() - 0.5
 }
 
 type Game struct {
 	particles []*Particle
+	images    []*ebiten.Image
+}
+
+func NewColor(r, g, b, a uint8) color.RGBA {
+	return color.RGBA{
+		R: r,
+		G: g,
+		B: b,
+		A: a,
+	}
+
 }
 
 func NewGame() *Game {
+	images := make([]*ebiten.Image, 0)
+	image := ebiten.NewImage(10, 10)
+	image.Fill(NewColor(0xff, 0xff, 0xff, 0xa0))
+	images = append(images, image)
 	particles := make([]*Particle, 0)
 	for i := 0; i < 100; i++ {
-		particles = append(particles, &Particle{x: rand.Float64() * 320, y: rand.Float64() * 240})
+		particles = append(particles, &Particle{x: rand.Float64() * 320, y: rand.Float64() * 240, img: images[0]})
 	}
 	return &Game{
 		particles: particles,
@@ -31,6 +53,9 @@ func NewGame() *Game {
 }
 
 func (g *Game) Update() error {
+	for _, particle := range g.particles {
+		particle.update()
+	}
 	return nil
 }
 
